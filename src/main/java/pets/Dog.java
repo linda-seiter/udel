1240863030074203f8f3ddfbc6f8f7ebe73bba68
Linda Seiter <linseiter@gmail.com>
2024-01-31 19:48:37 -0500

    update Dog example

--- src/main/java/pets/Dog.java
+++ src/main/java/pets/Dog.java
@@ -7,43 +7,25 @@ public class Dog {
     private boolean isWaggingTail, likesBaths;
 
     //Method declarations
-    public void treat(int quantity) { this.isWaggingTail = quantity > 2; }
-    public void bathe() {
-        this.isWaggingTail = this.likesBaths;
-    }
     public void scold() {this.isWaggingTail = false;}
-
-    @Override
-    public String toString() {
-        return String.format("name:%s wagging:%b likes baths:%b",
-                this.name, this.isWaggingTail, this.likesBaths);
-    }
+    public void treat(int quantity) { this.isWaggingTail = quantity > 2; }
+    public void bathe() {this.isWaggingTail = this.likesBaths;}
 
     public static void main(String[] args) {
 
         Dog dog1 = new Dog();
         Dog dog2 = new Dog();
 
-        //Update object fields
         dog1.name = "Bella";
         dog2.name = "Pepper";
         dog2.likesBaths = true;
 
         //Call instance methods
-        System.out.println(dog1);  //calls toString()
-        dog1.treat(5); //name:Bella wagging:false likes baths false
-
-        System.out.println(dog1);
-        dog1.bathe();
-        System.out.println(dog1);
-
-        System.out.println(dog2);
-        dog2.bathe();
-        System.out.println(dog2);
-        dog2.treat(1);
-        System.out.println(dog2);
-        dog2.treat(3);
-        System.out.println(dog2);
+        dog1.treat(5); //wagging true
+        dog1.bathe();  //wagging false
+        dog2.bathe();  //wagging true
+        dog2.treat(1); //wagging false
+        dog2.treat(3); //wagging true
+        dog2.scold(); //wagging false
     }
-
 }
